0e29765c83411913901385dad174ee37dc444b5a
calebkabaya <calebkabaya@gmail.com>
2022-05-01 00:22:17 +0300

    bug fix

--- app/models.py
+++ app/models.py
@@ -8,3 +8,27 @@ class Source:
         self.name=name
         self.description=description
         self.url=url
+
+class Headlines:
+    '''
+    Class for headlines categories o
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title  
+
+class Article:
+    '''
+    Class that instantiates objects of the news article objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title              
