2ad2994e2469d5bcd90f59d940d86d678657acf9
calebkabaya <calebkabaya@gmail.com>
2022-05-01 00:21:47 +0300

    st:added the a vies function for the articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,url_for
 from . import main
 from flask import render_template,request,redirect,url_for
-from ..request import get_news
+from ..request import get_news,article_source,get_category,get_headlines
 
 @main.route('/')
 def index():
@@ -11,3 +11,13 @@ def index():
     """
     source= get_news()
     return render_template('index.html',sources=source)
+
+@main.route('/article/<id>') 
+def article(id):
+    """
+    view the article page
+    """
+    articles=article_source(id)
+    return render_template('article.html',articles= articles,id=id )
+
+
