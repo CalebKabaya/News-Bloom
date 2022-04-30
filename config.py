43a38766775784b1894e925057fddfa22a312514
calebkabaya <calebkabaya@gmail.com>
2022-04-30 12:07:02 +0300

    bug fix on flask module compact

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+class Config:
+
+      # https://newsapi.org/v2/everything?
+    NEWS_API_URL='https://newsapi.org/v2/sources?apiKey={}'
+
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY') 
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
